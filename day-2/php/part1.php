49fc6ec86e59bef9253b941f223a0d171d59cf07
Ruslan Ishemgulov <ruslan.ishemgulov@gmail.com>
2023-12-02 23:31:15 +0100

    Day 2: php

---
+++ day-2/php/part1.php
@@ -0,0 +1,41 @@
+<?php
+
+$limits = [
+	'red' => 12,
+	'blue' => 14,
+	'green' => 13
+];
+
+$result = [];
+
+$games = file('../input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$pattern = '/(\d+ |blue|red|green)/';
+
+$colors = [];
+
+foreach($games as $index => $game) {
+	$gameKey = $index+1;
+
+	$colors[$index] = [];
+
+	list($round, $cubes) = explode(": ", $game);
+
+	preg_match_all('/\d+ (blue|red|green)/', $cubes, $matches);
+
+	foreach($matches[0] as $value) {
+		list($count, $color) = explode(" ", $value);
+
+		if($limits[$color] >= $count) {
+			$result[$gameKey] = true;
+		} else {
+			$result[$gameKey] = false;
+			break;
+		}
+	}
+}
+
+var_dump(array_sum(array_keys(array_filter($result, function($item) {
+	return $item === true;
+}))));
+
---
+++ day-2/php/part2.php
@@ -0,0 +1,43 @@
+<?php
+
+$inputFile = '../input.txt';
+
+function calculate($inputFile) {
+    $games = file($inputFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $totalPower = 0;
+
+    foreach ($games as $game) {
+        $matches = [];
+        if (preg_match('/Game (\d+): (.+)/', $game, $matches)) {
+            $game = $matches[2];
+
+            $rounds = explode(';', $game);
+            $colors = [
+                'red' => 0,
+                'green' => 0,
+                'blue' => 0,
+            ];
+
+            foreach ($rounds as $round) {
+                $cubes = explode(',', $round);
+
+                foreach ($cubes as $cube) {
+                    preg_match('/(\d+)(.*)/', $cube, $matches);
+                    $count = intval($matches[1]);
+                    $color = trim($matches[2]);
+
+                    $colors[$color] = max($colors[$color], $count);
+                }
+            }
+
+            $power = $colors['red'] * $colors['green'] * $colors['blue'];
+            $totalPower += $power;
+        }
+    }
+
+    return $totalPower;
+}
+
+$totalPower = calculate($inputFile);
+
+echo $totalPower . PHP_EOL;
